7eab20426f56bc5e88a4faf5c664da7cc615d580
Adam Dziomdziora <190140@edu.p.lodz.pl>
2018-11-17 19:54:14 +0100

    packages upgrade

--- DataProcessing/AutomapperConfig.cs
+++ DataProcessing/AutomapperConfig.cs
@@ -4,32 +4,22 @@ namespace DataProcessing
 {
     public static class AutoMapperConfig
     {
+        private static int TryParseStringToInt(string parser)
+        {
+            int.TryParse(parser, out int parsed);
+            return parsed;
+        }
+
         public static void Initialize()
         {
             Mapper.Initialize(cfg =>
             {
                 cfg.CreateMap<InputClass, OutputClass>()
-                    .ForMember(dest => dest.Year_of_Release, x => x.ResolveUsing(src =>
-                    {
-                        int.TryParse(src.Year_of_Release, out int yearOfRelease);
-                        return yearOfRelease;
-                    })).ForMember(dest => dest.Critic_Score, x => x.ResolveUsing(src =>
-                    {
-                        int.TryParse(src.Critic_Score, out int critScore);
-                        return critScore;
-                    })).ForMember(dest => dest.User_Score, x => x.ResolveUsing(src =>
-                    {
-                        int.TryParse(src.User_Score, out int userScore);
-                        return userScore;
-                    })).ForMember(dest => dest.Critic_Count, x => x.ResolveUsing(src =>
-                    {
-                        int.TryParse(src.Critic_Count, out int critCount);
-                        return critCount;
-                    })).ForMember(dest => dest.User_Count, x => x.ResolveUsing(src =>
-                    {
-                        int.TryParse(src.User_Count, out int userCount);
-                        return userCount;
-                    }));
+                    .ForMember(dest => dest.Year_of_Release, x => x.MapFrom(src => TryParseStringToInt(src.Year_of_Release)))
+                    .ForMember(dest => dest.Critic_Score, x => x.MapFrom(src => TryParseStringToInt(src.Critic_Score)))
+                    .ForMember(dest => dest.User_Score, x => x.MapFrom(src => TryParseStringToInt(src.User_Score)))
+                    .ForMember(dest => dest.Critic_Count, x => x.MapFrom(src => TryParseStringToInt(src.Critic_Count)))
+                    .ForMember(dest => dest.User_Count, x => x.MapFrom(src => TryParseStringToInt(src.User_Count)));
             });
         }
     }
